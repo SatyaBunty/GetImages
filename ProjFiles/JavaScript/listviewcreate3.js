fc2e1b530d588aea0941135d892de8daaa0b7f69
SatyaBunty <31646094+SatyaBunty@users.noreply.github.com>
2018-05-09 16:39:13 +0530

    Update listviewcreate3.js

--- ProjFiles/JavaScript/listviewcreate3.js
+++ ProjFiles/JavaScript/listviewcreate3.js
@@ -205,3 +205,33 @@ function UpdateModel()
   var modelval = document.getElementById('ModelOptions');
   urlId.value = (urlval.value)+(modelval.value);
 }
+
+function myFunction()
+{
+  var url = "../LocalImages";
+  var localURL = "ProjFiles/LocalImages/";
+  var obj = { "localURL":url };
+  dbParam = JSON.stringify(obj);
+  var xobj = new XMLHttpRequest();
+    // xobj.overrideMimeType("application/json");
+    xobj.onreadystatechange = function () 
+    {
+      if (xobj.readyState == 4 && xobj.status == 200)
+      {
+        // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+        //callback(xobj.responseText);
+        var data = xobj.response;
+        var options = JSON.parse(data);
+        var optionsHTML="";
+        for(i=0; i<options.all_local_files.length; i++ )
+        {
+          data += "<div id=\"lvtemplate\" style=\" position:static; margin:auto; padding-top: 12px; padding-right: 13px; padding-bottom: 16px; padding-left: 15px; height:400px; width:160px; float:left; vertical-align:middle\">"+"\n"+"<center>"+"\n"+"<h2>"+"\n"+" hi, hello"+"\n"+"</h2>"+"\n"+"<a href=\""+localURL+options.all_local_files[i]+"\" >"+"\n"+"<img style=\"height : 200px; width : 200px; float:left\"src=\""+localURL+options.all_local_files[i]+"\" />"+"\n"+"</a>"+"\n"+"<h3>"+"\n"+"see you..."+"\n"+"</h3>"+"\n"+"</center>"+"\n"+"</div>";
+        }
+        var y=document.getElementById("listview");
+        y.innerHTML = data;
+      }
+    };
+    xobj.open("POST", "./ProjFiles/PHPServices/ReceiveService.php?userRequest=GetLocalImages", true);
+    xobj.setRequestHeader("Content-type", "application/json");
+    xobj.send(dbParam);
+}
